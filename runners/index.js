3a04861fb0174e102f5e6a6951127251327437fd
Sahithyan <sahithyan2701@gmail.com>
2021-01-23 23:23:27 +0530

    wrote tests for runners in general

---
+++ runners/index.js
@@ -0,0 +1,4 @@
+export { isPrime } from "./is-prime.mjs"
+export { factorize } from "./factorize"
+export { gcd } from "./gcd"
+export { lcm } from "./lcm"
---
+++ tests/runners.test.js
@@ -0,0 +1,15 @@
+import * as RUNNERS_OBJ from "./../runners/index.js"
+
+const RUNNERS_ARRAY = Object.keys(RUNNERS_OBJ).map((key) => [
+	key,
+	RUNNERS_OBJ[key],
+])
+
+describe("Runner.meta", () => {
+	RUNNERS_ARRAY.forEach(([runnerName, runnerFunc]) => {
+		test(runnerName, () => {
+			expect(runnerFunc.meta).not.toBe(undefined)
+			// TODO write tests for properties of meta
+		})
+	})
+})
